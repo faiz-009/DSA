feab424b5c76551e1156e06502d4b8fcaf773fc3
MOHD. FAIZ QURESHI <mdfaizqureshi09@gmail.com>
2022-08-10 15:57:12 +0530

    Update 03_printAllDuplicates.py

--- LoveBabbar/String/03_printAllDuplicates.py
+++ LoveBabbar/String/03_printAllDuplicates.py
@@ -1,2 +1,19 @@
+# input: geeks
+# output: g, 1
+#         e, 2
+#         k, 1
+#         s, 1
+
 s = input()
 n = len(s)
+ans = {}
+
+for i in range(n):
+    if s[i] not in ans:
+        ans[s[i]] = 1
+    else:
+        a = ans[s[i]]
+        ans[s[i]] = a+1
+    
+print(ans)
+
